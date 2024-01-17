351b91378467b8fe1bb05314622e7d349bc78366
mgsmite <107347717+mgsmite@users.noreply.github.com>
2024-01-17 16:00:08 +0000

    commit

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 document.getElementById("yt-button").onclick = function () {
-    location.href = "https://www.youtube.com/@LeavesOfPower"
+    location.href = "https://www.youtube.com/@LeavesOfPower";
+};
+
+document.getElementById("snake").onclick = function () {
+    location.href = "/games/snake/snake.html";
 };
